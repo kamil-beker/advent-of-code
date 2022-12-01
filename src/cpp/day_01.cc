313f93ee3cbba6291a75e74aaca3c708ab386d76
kamil-beker <kamil@fourtheon.pl>
2022-12-01 09:51:43 +0100

    :recycle: - refactor namings

--- src/cpp/day_01.cc
+++ src/cpp/day_01.cc
@@ -20,18 +20,18 @@ std::int64_t Part01(std::vector<std::vector<std::int32_t>> data) {
 }
 
 std::int64_t Part02(std::vector<std::vector<std::int32_t>> data) {
-  std::int64_t acc_sum = 0;
+  std::int64_t max = 0;
   std::vector<std::int32_t> sums;
 
   for (const auto& i : data) {
-    const std::int32_t sum = std::accumulate(i.begin(), i.end(), 0);
+    auto sum = std::accumulate(i.begin(), i.end(), 0);
     sums.push_back(sum);
   }
 
   std::sort(sums.begin(), sums.end(), std::greater<std::int32_t>());
-  acc_sum = std::accumulate(sums.begin(), sums.begin() + 3, 0);
+  max = std::accumulate(sums.begin(), sums.begin() + 3, 0);
 
-  return acc_sum;
+  return max;
 }
 
 }  // namespace day01
